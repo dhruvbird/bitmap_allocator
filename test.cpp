3b362bbfd7eb016bf79343d2873431b4de272947
Dhruv Matani <dhruvbird@fb.com>
2012-07-25 11:29:14 -0700

    Make test more configurable by defining macros while compiling

--- test.cpp
+++ test.cpp
@@ -1,13 +1,18 @@
+// -*- mode:c++; c-basic-offset:4 -*-
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include <vector>
 #include <memory>
 #include <list>
-#define NDEBUG
 
+#if defined BALLOC
 #include "bitmap_allocator.h"
+#elif defined EXT_BALLOC
+#include <ext/bitmap_allocator.h>
+#endif
 
 using namespace std;
 using namespace __gnu_cxx;
@@ -16,8 +21,11 @@ std::vector<int*> vpi;
 std::vector<int> numbers;
 
 void test_list(int n) {
-  typedef bitmap_allocator<int> alloc_t;
-  //   typedef std::allocator<int> alloc_t;
+#if defined BALLOC || defined EXT_BALLOC
+    typedef bitmap_allocator<int> alloc_t;
+#else
+    typedef std::allocator<int> alloc_t;
+#endif
     typedef std::list<int, alloc_t> list_t;
     list_t il;
 
@@ -29,8 +37,11 @@ void test_list(int n) {
 }
 
 void test_alloc(int n) {
-  bitmap_allocator<int> ia;
-  //  std::allocator<int> ia;
+#if defined BALLOC || defined EXT_BALLOC
+    bitmap_allocator<int> ia;
+#else
+    std::allocator<int> ia;
+#endif
 
     for (int i = 0; i < n-1; ++i) {
         vpi.push_back(ia.allocate(1));
@@ -45,7 +56,7 @@ void test_alloc(int n) {
 }
 
 int
-main() {
+main(int argc, char *argv[]) {
     vpi.reserve(1<<23);
     numbers.reserve(1<<23);
 
@@ -54,8 +65,14 @@ main() {
     }
 
     for (int i = 0; i < 24; ++i) {
-      test_alloc(1<<i);
-      // test_list(1<<i);
+#if defined TEST_ALLOC
+	test_alloc(1<<i);
+#else
+	test_list(1<<i);
+#endif
     }
-    // sleep(50);
+    std::string command = "ps aux | grep ";
+    command += argv[0];
+    int ret = system(command.c_str());
+    (void)ret;
 }
